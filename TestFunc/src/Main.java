b7609989ecdc89ada26481cefc26f3df0ce8a26d
huaguang <huaguangyanmie@163.com>
2016-04-12 08:47:41 +0800

    test_commit

--- TestFunc/src/Main.java
+++ TestFunc/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
-		new MyServerSocket();
+		//new MyServerSocket();
 
 	}
 
