6c87c7ec01dfd7553189aa344259a25d867f59e7
huagu <huagu@billSSecondPE.lan>
2016-04-12 08:53:48 +0800

    MyServerSocket_1

---
+++ TestFunc/src/MyServerSocket.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MyServerSocket {
+	public MyServerSocket() throws IOException
+	{
+		ServerSocket server=new ServerSocket(8888);
+		Socket client=server.accept();
+		String str=new String("Hello world!");
+		OutputStream out=client.getOutputStream();
+		PrintStream print=new PrintStream(out);
+		print.print(str);
+		client.close();
+		server.close();
+	}
+
+}
